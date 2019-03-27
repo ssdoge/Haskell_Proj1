84470b68b18cf8b50bfd82dea51efb4f63daf641
jrs <sjrthu@gmail.com>
2019-03-27 22:59:15 +1100

    Update proj1.hs
    模式枚举

--- proj1.hs
+++ proj1.hs
@@ -1,19 +1,21 @@
 data Note = A | B | C | D | E | F | G deriving (Eq,Show)
--- data Octave = One | Two | Three deriving Eq
--- data Pitch = Pitch Note Octave deriving Eq 
+data Octave = One | Two | Three deriving Eq
+data Pitch = Pitch Note Octave deriving Eq 
 
 -- toPitch
-toNote :: Char ->Maybe Note
-toNote char = case char of  
-	'A' -> Just A
-	'B' -> Just B
-	otherwise -> Nothing
-toOctave :: Char ->Maybe Octave
-toOctave num = case num of
-	'1' -> Just One
-	'2' -> Just Two
-	'3' -> Just Three
-	otherwise -> Nothing
+isNote :: Char ->Bool
+isNote char = case char of  
+	'A' -> True
+	'B' -> True
+	otherwise -> False
+isOctave :: Char ->Bool 
+isOctave num = case num of
+	'1' -> True
+	'2' -> True
+	'3' -> True
+	otherwise -> False
 toPitch :: String -> Maybe Pitch
-toPitch x:y:[] 
+toPitch x:y:[] 	
+	| (isNote x) == True && (isOctave y) == True = Pitch 
+  
 toPitch _ = Nothing
