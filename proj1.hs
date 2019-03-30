b03c43401feb87324276710b1020c1e8335a8df1
jrs <sjrthu@gmail.com>
2019-03-30 19:16:13 +1100

    Update proj1.hs
    finished : octaveGuess

--- proj1.hs
+++ proj1.hs
@@ -6,7 +6,8 @@ import Data.List
 data Note = A | B | C | D | E | F | G deriving (Eq,Ord,Show,Read)
 data Octave = One | Two | Three deriving (Eq,Ord,Read,Show)
 data Pitch = Pitch Note Octave deriving (Eq,Ord)
-data GameState = ()
+data OctaveState = OctaveState Bool Int 
+data GameState = GameState OctaveState
 instance Show Pitch where show = showPitch
 
 testPitch1 = [(Pitch A Two), (Pitch F One) , (Pitch D Three) ]
@@ -22,23 +23,30 @@ testP00 = map fromJust (map toPitch ["A1","B2","A3"])
 testP000 = map fromJust (map toPitch ["A1","A2","B1"])
 
 
--- Define the tables for changing datatpye
+--define the tables for changing datatpye
 tableOfoctave = [(One,'1'),(Two,'2'),(Three,'3')]
 tableOfNote = [(A,'A'),(B,'B'),(C,'C'),(D,'D'),(E,'E'),(F,'F'),(G,'G')]
 
+--get Note&Octave from Pitch
+getNote :: Pitch -> Note
+getNote (Pitch n _ ) = n 
+
+getOctave :: Pitch -> Octave
+getOctave (Pitch _ o ) = o
+ 
 ------------------Display-------------------------------------------------
--- functions for changing datatype from Octave <-> Char
+--functions for changing datatype from Octave <-> Char
 fromOctave :: Octave -> Char
 fromOctave x = fromJust (lookup x tableOfoctave)
 
 toOctave :: Char -> Octave
 toOctave x = fromJust (lookup x (map swap tableOfoctave))
 
--- show function for Pitch
+--show function for Pitch
 showPitch :: Pitch -> String
 showPitch (Pitch n o) =	((head.show) n):(fromOctave o):[] 
 
--- change datatpye from String to Pitch
+--change datatpye from String to Pitch
 toPitch :: String -> Maybe Pitch
 toPitch (x:y:[])
 	| isJust (lookup x (map swap tableOfNote)) 
@@ -113,10 +121,40 @@ feedBack target guess =
 
 initialGuess :: ([Pitch],GameState)
 initialGuess = (map fromJust (map toPitch ["A1","B2","C3"]) , 
-				())
+				(GameState (OctaveState False 1 )) )
 
+octaveGuess :: ([Pitch],OctaveState) -> (Int, Int, Int) 
+					-> (Octave, Octave, Octave, OctaveState)
+octaveGuess (p1:p2:p3:_, OctaveState b kth) (pNum, _, oNum) =
+	if b == True then (getOctave p1, getOctave p2, getOctave p3,
+						 (OctaveState True (kth+1) ))
+		else case num of 
+			3 -> (getOctave p1, getOctave p2, getOctave p3,
+						 (OctaveState True (kth+1) ))
+			2 -> case kth of 
+				1 -> (One, Two, Two, (OctaveState False (kth+1)))
+				2 -> (One, One, Two, (OctaveState False (kth+1)))
+				3 -> (One, Three, Three, (OctaveState False (kth+1)))
+			1 -> case kth of 
+				1 -> (One, One, One, (OctaveState False (kth+1)))
+				2 -> (Two, Three, Three, (OctaveState False (kth+1)))
+				3 -> case (getOctave p1, getOctave p2, getOctave p3) of 
+					(Two, Three, Three) -> (One, One, Three,
+											 (OctaveState False (kth+1)))
+					(One, One, Two) -> (Two, Two, Three,
+										 (OctaveState False (kth+1)))
+			0 -> case kth of
+				2 -> (Two, Two, Two, (OctaveState False (kth+1)))
+				3 -> (Three, Three, Three, (OctaveState False (kth+1)))
+
+			otherwise -> error "error octaveGuess"
+	where num = pNum+oNum
+
+
+{-
 nextGuess :: ([Pitch],GameState) → (Int,Int,Int) → ([Pitch],GameState)
-nextGuess 
+nextGuess (pitches, gs) (pNum, nNum, oNum) =
+-}
 
 
 
